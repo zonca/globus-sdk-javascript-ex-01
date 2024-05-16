c4c4d354dda2ced5c02c142d36c9bdcf3996f287
Andrea Zonca <code@andreazonca.com>
2024-05-16 13:24:14 -0700

    test with button

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from pyscript import display
 import pandas as pd
-from js import createObject
-from pyodide import create_proxy, to_js
 
 def pandas_plot():
-    from js import data
-    df = pd.DataFrame(data)
-    df = df.set_index("year")
-    display(df.plot(kind="bar"), target="chart")
-
-createObject(create_proxy(pandas_plot), "pandas_plot_js")
+    # from js import data
+    # df = pd.DataFrame(data)
+    # df = df.set_index("year")
+    # display(df.plot(kind="bar"), target="chart")
+    from datetime import datetime
+    now = datetime.now()
+    display(now.strftime("%m/%d/%Y, %H:%M:%S"), target="chart")
