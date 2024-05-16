07b9932f233f28367736ce2f418c505d62bd3b57
Andrea Zonca <code@andreazonca.com>
2024-05-16 14:46:48 -0700

    pyscript version working

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pyscript import display
+from js import console
 import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
 
-def pandas_plot():
-    # from js import data
-    # df = pd.DataFrame(data)
-    # df = df.set_index("year")
-    # display(df.plot(kind="bar"), target="chart")
-    from datetime import datetime
-    now = datetime.now()
-    display(now.strftime("%m/%d/%Y, %H:%M:%S"), target="chart")
+def pandas_plot(*args):
+    from js import data
+    console.log(data)
+    df = pd.read_json(StringIO(data))
+    df = df.set_index("year")
+    fig, ax = plt.subplots()
+    df.plot(kind="bar", ax=ax)
+    display(fig, target="chart")
